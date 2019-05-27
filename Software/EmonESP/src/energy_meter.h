caad3154f0a63caea7fdbf3583c46f20e2677872
John <34163498+CircuitSetup@users.noreply.github.com>
2019-05-27 13:01:26 -0400

    set default values

--- Software/EmonESP/src/energy_meter.h
+++ Software/EmonESP/src/energy_meter.h
@@ -8,11 +8,11 @@
 // 0 for 10A (1x)
 // 21 for 100A (2x)
 // 42 for between 100A - 200A (4x)
-#define PGA_GAIN 42
+#define PGA_GAIN 21
 
 // 41820 - 9v AC transformer
 // 32428 - 12v AC Transformer
-#define VOLTAGE_GAIN 42483
+#define VOLTAGE_GAIN 41820
 
 // 25498 - SCT-013-000 100A/50mA
 // 46539 - Magnalab 100A/200A
@@ -20,7 +20,7 @@
 
 // 25498 - SCT-013-000 100A/50mA
 // 46539 - Magnalab 100A/200A
-#define CURRENT_GAIN_CT2 46539
+#define CURRENT_GAIN_CT2 25498
 
 extern void energy_meter_setup();
 extern void energy_meter_loop();

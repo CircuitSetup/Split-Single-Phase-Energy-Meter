8a00a36b6146169b27ae47bfeead8c01ec4a410b
John <34163498+CircuitSetup@users.noreply.github.com>
2019-10-09 15:55:42 -0400

    Update web_server.h
    moved hash.h to compile with esp8266 only

--- Software/EmonESP/src/web_server.h
+++ Software/EmonESP/src/web_server.h
@@ -30,12 +30,13 @@
 
 #include <FS.h>                       // SPIFFS file-system: store web server html, CSS etc.
 #include <Arduino.h>
-#include <Hash.h>
+
 #ifdef ESP32
 #include <SPIFFS.h>
 #include <WiFi.h>
 #include <AsyncTCP.h>  //https://github.com/me-no-dev/AsyncTCP
 #elif defined(ESP8266)
+#include <Hash.h>
 #include <ESP8266WiFi.h>
 #include <ESPAsyncTCP.h>
 #endif
